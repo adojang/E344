e21672dc33d00f3b349ffd9a204d9dc14bb00731
catswithhats <masterkittypaws@gmail.com>
2020-10-02 14:56:22 +0200

    Assignment 3
    Start of the final push

---
+++ LTSpice/Assignment 3/student_heart.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def calibrate(time, amplitude):
+        
+    ######################################
+    # Enter calibration code here:
+    ######################################
+        
+    bpm = amplitude[0] # This line will is to be replaced with your calibration code.
+         
+        
+        
+        
+    ######################################
+        
+    return bpm
+        
---
+++ LTSpice/Assignment 3/student_temp.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def calibrate(time, amplitude):
+        
+    ######################################
+    # Enter calibration code here:
+    ######################################
+        
+    temperature = amplitude[0] # This line will is to be replaced with your calibration code.
+        
+        
+        
+        
+    ######################################
+        
+    return temperature
+        
