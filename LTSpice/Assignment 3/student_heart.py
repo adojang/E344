34f8e6d8e8a79c8b4f952d6354d137bb7aa7cdc7
catswithhats <masterkittypaws@gmail.com>
2020-10-04 12:57:28 +0200

    Everything works Perfectly
    Don't touch anything

--- LTSpice/Assignment 3/student_heart.py
+++ LTSpice/Assignment 3/student_heart.py
@@ -4,21 +4,42 @@ def calibrate(time, amplitude):
         
     ######################################
     # Enter calibration code here:
+    # AJ van Wijk 21786275
     ######################################
-        
-    samples = 2000
 
-    for x in range (0, samples)
-    	if amplitude[0] > 2.5
-    		liquid = 1
+    samples = len(amplitude)
+    newpulse =0
+    oldstate = True
+    newstate = False
+    oldpulse =0
+    counter =0
+    totalperiod =0
 
+    for x in range(samples):
 
+       if amplitude[x] > 2.5:
 
-    bpm = amplitude[0]
-        
-        
-        
+          newstate = True
+       else:
+          newstate = False
+
+       if oldstate == False and newstate == True: #Change from High to Low happens Here or Low to High
+
+          newpulse = x
+          period = newpulse - oldpulse
+          if counter > 0: #This disregards the first value which is not a viable value.
+             totalperiod += period
+          
+          counter += 1 #Counter keeps track of how many pulses there are
+          
+       #These allow us to calculate the difference
+       oldstate = newstate
+       oldpulse = newpulse
+
+    avg = (totalperiod/(counter-1))/1000 #Gives us the average period in SECONDS
+    bpm = 60/avg #Changes seconds to BPM
+
     ######################################
-        
-    return bpm
+  
+    return int(round(bpm))
         
--- LTSpice/Assignment 3/student_temp.py
+++ LTSpice/Assignment 3/student_temp.py
@@ -4,6 +4,7 @@ def calibrate(time, amplitude):
         
     ######################################
     # Enter calibration code here:
+    # AJ van Wijk 21786275
     ######################################
         
     x = (amplitude[0]-0.446)*2.131 + 34 # This line will is to be replaced with your calibration code.
